9f520fed18b638fb59548d5d4b9a23f1ee29cd1e
Jose Escudero <joseignacioescudero@gmail.com>
2022-10-16 17:04:57 -0500

    add assertions

--- py_startup.py
+++ py_startup.py
@@ -1,5 +1,5 @@
 from config import StagingProperties
-from extract import truncate, extract
+from extract import countries, truncate, extract
 from utils.db_connection import Db_Connection
 
 
@@ -11,4 +11,27 @@ with db_context.begin(): # transaction
         truncate(db_context)
         extract(db_context)
 
+
+channels_count = db_context.execute('SELECT COUNT(*) FROM CHANNELS').scalar()
+assert channels_count == 5
+
+countries_count = db_context.execute('SELECT COUNT(*) FROM COUNTRIES').scalar()
+assert countries_count == 23
+
+customers_count = db_context.execute('SELECT COUNT(*) FROM CUSTOMERS').scalar()
+assert customers_count == 55500
+
+products_count = db_context.execute('SELECT COUNT(*) FROM PRODUCTS').scalar()
+assert products_count == 72
+
+promotions_count = db_context.execute('SELECT COUNT(*) FROM PROMOTIONS').scalar()
+assert promotions_count == 503
+
+sales_count = db_context.execute('SELECT COUNT(*) FROM SALES').scalar()
+assert sales_count == 918843
+
+times_count = db_context.execute('SELECT COUNT(*) FROM TIMES').scalar()
+assert times_count == 1826
+
 db_context.dispose()
+
